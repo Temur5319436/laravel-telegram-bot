2e939a51aa71e43b43c71fab2da72c2ed6624d58
Temur5319436 <Temur5319436@gmail.com>
2021-06-26 19:45:56 +0500

    24

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -7,14 +7,14 @@ use App\Http\Requests\TelegramRequest;
 
 class MessageController extends Controller
 {
-    public function api(TelegramRequest $request)
+    public function api(TelegramRequest $telegramRequest)
     {
         $telegram = new Telegram(env('TELEGRAM_BOT_TOKEN'));
         $chatId = $telegram->ChatID();
 
         $telegram->sendMessage([
             'chat_id' => $chatId,
-            'text' => json_encode($request->toArray(), 128)
+            'text' => json_encode($telegramRequest->toArray(), 128)
         ]);
     }
 }
