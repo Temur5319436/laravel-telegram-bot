06edcf3b0e2e69120f47679b8c6580aae48492d0
Temur5319436 <Temur5319436@gmail.com>
2021-06-26 21:58:54 +0500

    28

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MessageController;
+use App\Http\Controllers\MainController;
 
 
 Route::post('/42yUojv1YQPOssPEpn5i3q6vjdhh7hl7djVWDIAVhFDRMAwZ1tj0Og2v4PWyj4PZ/webhook', [MainController::class, 'api']);
