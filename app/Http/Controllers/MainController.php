5d38b84dbfb2ecfb06fb5404f73aa7640c251b6e
Temur5319436 <Temur5319436@gmail.com>
2021-06-27 11:29:22 +0500

    69

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -17,24 +17,24 @@ class MainController extends Controller
 
     public function api(TelegramRequest $request)
     {
-        try {
-            // $chatId = $this->telegram->ChatID();
-            // $data = $this->telegram->getData();
+        $chatId = $this->telegram->ChatID();
+        $data = $this->telegram->getData();
 
+        try {
             $this->telegram->sendMessage([
                 'chat_id' => 1622751454,
                 'text' => 'json_encode($data, 128)'
             ]);
         } catch (Exception $exception) {
             // --------- Error -------- //
-            // $this->telegram->sendMessage([
-            //     'chat_id' => $chatId,
-            //     'text' => json_encode([
-            //         'message' => $exception->getMessage(),
-            //         'file' => $exception->getFile(),
-            //         'line' => $exception->getLine()
-            //     ], 128)
-            // ]);
+            $this->telegram->sendMessage([
+                'chat_id' => $chatId,
+                'text' => json_encode([
+                    'message' => $exception->getMessage(),
+                    'file' => $exception->getFile(),
+                    'line' => $exception->getLine()
+                ], 128)
+            ]);
         }
     }
 }
